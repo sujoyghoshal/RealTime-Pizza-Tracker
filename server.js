bfac76d8987e6f5b0d6d5bb6ea4e9a9ccb47ceee
sujoyghoshal <sujoyghoshal.s@gmail.com>
2024-04-22 11:49:51 +0530

    adding server.js connection to admin and user page

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const passport = require('passport')
 const Emitter = require('events')
 
 // Database connection
-mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+     useNewUrlParser: true,
+     useCreateIndex:true,
+     useUnifiedTopology: true, 
+     useFindAndModify : true 
+    });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
