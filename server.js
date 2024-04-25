9f66fa6d5838b36af1bf346053ac23c9fd57ce68
sujoyghoshal <sujoyghoshal.s@gmail.com>
2024-04-25 11:30:36 +0530

    adding the server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL, {
      useNewUrlParser: true,
      useCreateIndex:true,
      useUnifiedTopology: true, 
-     useFindAndModify : true 
+     useFindAndModify : true  
     });
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -26,7 +26,7 @@ connection.once('open', () => {
     console.log('Connection failed...')
 });
 
-
+ 
 // Session store
 let mongoStore = new MongoDbStore({
                 mongooseConnection: connection,
